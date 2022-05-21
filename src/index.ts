5796ef78295fd73c377dcaaa62eded41e56b3f01
すずねーう <suzuneu@suzuneu.com>
2022-05-21 11:35:41 +0900

    cmt

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,46 @@
-import axios from "axios";
-import { type AxiosResponse } from "axios";
+import Axios from "axios";
+import type { AxiosResponse, AxiosInstance } from "axios";
 
 class PayPayError extends Error {
   public constructor(title: string, message: string) {
-    super(`[${title.toUpperCase()}] ${message}`);
-    this.name = this.constructor.name;
+    super(`${message}`);
+    this.name = this.constructor.name + ` [${title.toUpperCase()}]`;
   }
 };
 
-type PartialPartial<T extends object, K extends keyof T> = { [P in K]?: T[P] } & { [P in Exclude<keyof T, K>]-?: T[P]};
+type PartialPartial<T extends object, K extends keyof T> = { [P in K]?: T[P] } & { [P in Exclude<keyof T, K>]-?: T[P] };
 
-type PayPayLoginResult<O extends boolean> = {
+type PayPayResult<S extends string = string> = {
   header: {
-    resultCode: O extends true ? "S0000" : "S1004",
+    resultCode: S,
     resultMessage: string
-  },
+  }
+}
+
+type PayPayLoginResult<O extends boolean> = PayPayResult<O extends true ? "S0000" : "S1004"> & {
   payload: O extends true ? {
     accessToken: string,
     refreshToken: string
-  }:never
+  } : never
   error: O extends false ? {
     otpPrefix: string,
     otpReferenceId: string
-  }:never
+  } : never
 }
 
 const isSMSRequired = (data: Partial<PayPayLoginResult<boolean>>): data is PayPayLoginResult<false> => {
   return data.header?.resultCode === "S1004";
 }
+
+const getHeader = (accessToken: string) => {
+  return {
+    'User-Agent': 'PaypayApp/3.31.202202181001 CFNetwork/1126 Darwin/19.5.0',
+    'Authorization': `Bearer ${accessToken}`,
+    'Client-Type': 'PAYPAYAPP',
+    'Client-OS-Type': 'IOS',
+    'Client-Version': '3.31.0'
+  } as const;
+};
 
 enum PayPayLoginStatus {
   DONE = 0,
@@ -37,14 +50,18 @@ enum PayPayLoginStatus {
 class PayPay<L extends boolean = false> {
   public clientUuid: string;
   public deviceUuid: string;
+  public accessToken?: string;
   private _logged: L;
-  public constructor(clientUuid: string, deviceUuid: string) {
+  private readonly _axios: AxiosInstance
+  public constructor(clientUuid: string, deviceUuid: string, accessToken?: string) {
     this.clientUuid = clientUuid;
     this.deviceUuid = deviceUuid;
-    this._logged = false as L;
+    this.accessToken = accessToken;
+    this._logged = (accessToken ? true:false) as L; 
+    this._axios = Axios.create({ validateStatus: () => true });
   }
-  public async login(phoneNumber: string, password: string): Promise<{ status: PayPayLoginStatus.DONE, accessToken: string, refreshToken: string } | { status: PayPayLoginStatus.OTP_REQUIRED, otpPrefix: string, otpReferenceId: string }> {
-    const { data } = await axios.post<PartialPartial<PayPayLoginResult<boolean>, "error" | "payload">>("https://app3.paypay.ne.jp/bff/v1/signIn?payPayLang=ja", {
+  public async login(phoneNumber: string, password: string): Promise<{ readonly status: PayPayLoginStatus.DONE, accessToken: string, refreshToken: string } | { status: PayPayLoginStatus.OTP_REQUIRED, otpPrefix: string, otpReferenceId: string }> {
+    const { data } = await this._axios.post<PartialPartial<PayPayLoginResult<boolean>, "error" | "payload">>("https://app3.paypay.ne.jp/bff/v1/signIn?payPayLang=ja", {
       phoneNumber,
       password
     }, {
@@ -68,12 +85,58 @@ class PayPay<L extends boolean = false> {
         "Accept": "*/*",
         "Client-OS-Version": "13.5.1",
         "Network-Status": "WIFI",
-      },
-      validateStatus: () => true
+      } as const,
     });
     if (isSMSRequired(data)) {
       return { status: PayPayLoginStatus.OTP_REQUIRED, otpPrefix: data.error.otpPrefix, otpReferenceId: data.error.otpReferenceId };
     }
-    return { status: PayPayLoginStatus.DONE, accessToken: data.payload!.accessToken, refreshToken: data.payload!.refreshToken }
+    if (data.header.resultCode === "S0000") {
+      this._logged = true as L;
+      return { status: PayPayLoginStatus.DONE, accessToken: data.payload!.accessToken, refreshToken: data.payload!.refreshToken }
+    }
+    throw new PayPayError("INVALID_PASSWORD", "an invalid password provided.");
+  }
+  public async loginOtp(otpReferenceId: string, otp: string): Promise<{ readonly status: PayPayLoginStatus.DONE, accessToken: string, refreshToken: string }> {
+    const headers = {
+      "Host": "app3.paypay.ne.jp",
+      "Client-UUID": this.clientUuid,
+      "System-Locale": "ja",
+      "Device-UUID": this.deviceUuid,
+      "Accept-Encoding": "gzip,deflate,br",
+      "User-Agent": "PaypayApp/3.31.202202181001CFNetwork/1126Darwin/19.5.0",
+      "Client-Version": "3.31.0",
+      "Device-Name": "iPad8,3",
+      "Content-Length": "74",
+      "Connection": "keep-alive",
+      "Client-OS-Type": "IOS",
+      "Client-Mode": "NORMAL",
+      "Client-Type": "PAYPAYAPP",
+      "Accept-Language": "ja-jp",
+      "Timezone": "Asia/Tokyo",
+      "Content-Type": "application/json",
+      "Accept": "*/*",
+      "Client-OS-Version": "13.5.1",
+      "Network-Status": "WIFI",
+    } as const;
+    const { data } = await this._axios.post<PayPayResult<"S0000"> & { payload: { accessToken: string, refreshToken: string } }>("https://app3.paypay.ne.jp/bff/v1/signInWithSms?payPayLang=ja", {
+      otp, otpReferenceId
+    }, {
+      headers
+    });
+    if (data.header.resultCode === "S0000") {
+      this.accessToken = data.payload.accessToken;
+      this._logged = true as L;
+      return { status: PayPayLoginStatus.DONE, accessToken: data.payload.accessToken, refreshToken: data.payload.refreshToken }
+    }
+    throw new PayPayError("OTP_INVALID", "an invalid otp code is provided.");
+  }
+  protected checkToken(): this is PayPay<true> {
+    return !!this._logged;
+  }
+  public async getBalance() {
+    if (!this.checkToken()) throw new PayPayError("NOT_LOGGED", "access token has not set.");
+
   }
 }
+
+export { PayPay, PayPayError, PayPayLoginResult, PayPayLoginStatus, PayPayResult };
